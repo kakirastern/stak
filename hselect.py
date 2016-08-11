a740f4d7f46e577a5c0701d54220b5904579b027
Sara Ogaz <ogaz@grue.local.stsci.edu>
2016-08-11 16:21:35 -0400

    it's alive! Working version with astropy table formatting!

--- hselect.py
+++ hselect.py
@@ -20,7 +20,7 @@ from future.utils import viewitems
 # THIRD-PARTY
 import numpy as np
 from astropy.io import fits
-from astropy.table import Table
+from astropy.table import Table,Column
 
 
 class Hselect(object):
@@ -28,25 +28,29 @@ class Hselect(object):
 
     """
 
-    def __init__(self, filename_list, keyword_list, extension='all', verbose = True, expr="None", add_name=True):
+    def __init__(self, filename_list, keyword_list, extension='all', expr="None"):
         """
         Parameters
         ----------
-        filename_list
-        keyword_list
-        extension
-        verbose
+        filename_list : str
+        keyword_list : str
+        extension : str
         expr : str
-        add_name
         """
 
         self.filename_list = self.__filename_translate(filename_list)
         self.final_key_dict = {}
         self.keyword_list = keyword_list.split(",")
+        self.final_key_set = set()
         self.extension = extension
         self.expr = expr
+        self.table = Table()
 
         self.select()
+        self.__dict_to_table()
+
+    def return_table(self):
+        return self.table
 
     def select(self):
         """
@@ -78,9 +82,10 @@ class Hselect(object):
                 header = hdulist[ext].header
                 outer_key = '{}-{}'.format(filename, ext)
 
-                # check for expressions, can terminate and delete dictionary entry if expression false
+                # check for expressions, will not make dictionary entry if expression false
                 # would be nice to use interpreter pattern here for more advanced expressions,
-                # but for now I'll stick with just 'AND's
+                # but for now I'll stick with just 'AND's. check is expr is valid only once,
+                # earlier in code.
                 if self.expr != "None" and not self.__eval_keyword_expr(header, self.expr):
                     continue
 
@@ -91,18 +96,65 @@ class Hselect(object):
                     if '*' in search_keyword:
                         matches = wildcard_matches(header, search_keyword)
                         for match in matches:
-                            self.final_key_dict[outer_key][match] = header[match]
+                            self.final_key_dict[outer_key][match], data_type = to_number(header[match])
+                            self.final_key_set.add((match, data_type))
 
                     else:
                         if search_keyword in header:
-                            self.final_key_dict[outer_key][search_keyword] = header[search_keyword]
+                            self.final_key_dict[outer_key][search_keyword], data_type = \
+                                to_number(header[search_keyword])
+                            self.final_key_set.add((search_keyword, data_type))
 
                 if not self.final_key_dict[outer_key]:
                     del self.final_key_dict[outer_key]
 
             hdulist.close()
 
-        print(self.final_key_dict)
+    def __dict_to_table(self):
+        """Format the final dictionary of keyword matches into a masked astropy table
+
+        """
+
+        # NOPE, need to setup all numpy arrays first, then put into table, adding columns is apparently slow
+        file_ext_list = self.final_key_dict.keys()
+        num_rows = len(file_ext_list)
+        final_keyword_list = list(self.final_key_set)
+
+        array_list = [[elem.split('-')[0] for elem in file_ext_list],
+                      [int(elem.split('-')[1]) for elem in file_ext_list]]
+        col_names = ['Filename', 'ExtNumber']
+        data_types = ['S80', int]
+        # the bool mask array list will be off by index of 2
+        mask_list = []
+        dumb_indx_dict = {}
+
+        # make initial empty data and bool arrays
+        for indx, key_tuple in enumerate(final_keyword_list):
+            #make dumb keyword index dict, probably a better way to do this
+            dumb_indx_dict[key_tuple[0]] = indx
+            if key_tuple[1] == str:
+                data_type = 'S68'
+            else:
+                data_type = float
+
+            array_list.append(np.zeros(num_rows, dtype=data_type))
+            mask_list.append(np.ones(num_rows, dtype=bool))
+            col_names.append(key_tuple[0])
+            data_types.append(data_type)
+
+        # now fill in arrays and mask arrays
+        for indx, efile in enumerate(file_ext_list):
+            for (ikeyword,value) in viewitems(self.final_key_dict[efile]):
+                ikeyword_indx = dumb_indx_dict[ikeyword]
+                array_list[ikeyword_indx+2][indx] = value
+                mask_list[ikeyword_indx][indx] = False
+
+        # put arrays and masks together into one masked array
+        for indx,mask_arr in enumerate(mask_list):
+            array_list[indx+2] = np.ma.array(array_list[indx+2], mask=mask_arr)
+
+        # put everything into an astropy table
+        self.table = Table(array_list, names=tuple(col_names), dtype=tuple(data_types))
 
     @staticmethod
     def __eval_keyword_expr(header, str_expr):
@@ -128,45 +180,40 @@ class Hselect(object):
             if '=' in elem:
                 elem = elem.split("=")
                 keyword = elem[0].strip(" ")
-                value = to_number(elem[1].strip(" "))
+                value, data_type = to_number(elem[1].strip(" "))
                 if keyword not in header.keys():
-                    print ("{} not found".format(keyword))
                     return False
                 if header[keyword] != value:
                     return False
             elif '>' in elem:
                 elem = elem.split(">")
                 keyword = elem[0].strip(" ")
-                value = to_number(elem[1].strip(" "))
-                if keyword not in header.keys():
-                    print ("{} not found".format(keyword))
+                value, data_type = to_number(elem[1].strip(" "))
+                if keyword not in header.keys() or data_type == str:
                     return False
                 if header[keyword] <= value:
                     return False
             elif '>=' in elem:
                 elem = elem.split(">=")
                 keyword = elem[0].strip(" ")
-                value = to_number(elem[1].strip(" "))
-                if keyword not in header.keys():
-                    print ("{} not found".format(keyword))
+                value, data_type = to_number(elem[1].strip(" "))
+                if keyword not in header.keys() or data_type == str:
                     return False
                 if header[keyword] < value:
                     return False
             elif '<=' in elem:
                 elem = elem.split("<=")
                 keyword = elem[0].strip(" ")
-                value = to_number(elem[1].strip(" "))
-                if keyword not in header.keys():
-                    print ("{} not found".format(keyword))
+                value, data_type = to_number(elem[1].strip(" "))
+                if keyword not in header.keys() or data_type == str:
                     return False
                 if header[keyword] > value:
                     return False
             elif '<' in elem:
                 elem = elem.split("<")
                 keyword = elem[0].strip(" ")
-                value = to_number(elem[1].strip(" "))
-                if keyword not in header.keys():
-                    print ("{} not found".format(keyword))
+                value, data_type = to_number(elem[1].strip(" "))
+                if keyword not in header.keys() or data_type == str:
                     return False
                 if header[keyword] >= value:
                     return False
@@ -225,6 +272,6 @@ def wildcard_matches(header, wildcard_key):
 def to_number(s):
     try:
         s = float(s)
-        return s
+        return s, float
     except ValueError:
-        return s
+        return s, str
