7fe62b87ef37e72e99161de8e3cd0f8abe50cf07
Sara Ogaz <ogaz@moka.local.stsci.edu>
2016-08-22 16:12:33 -0400

    Changes for new project name, apcmd

--- apcmd/__init__.py
+++ apcmd/__init__.py
@@ -1,7 +1,7 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 
 """
-This is an Astropy affiliated package.
+IRAF replacement / command line tools collection.
 """
 
 # Affiliated packages may add whatever they like to this file, but
