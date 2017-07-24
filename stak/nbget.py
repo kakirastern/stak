d54aed6bdb98b5a1bd5bc40b143d5d45fab7b210
Joseph Hunkeler <jhunkeler@users.noreply.github.com>
2017-07-24 12:50:22 -0400

    Python-2-ify code (#99)

--- stak/nbget.py
+++ stak/nbget.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+import atexit
 import os
 import shutil
 import sys
@@ -14,7 +15,10 @@ except ImportError:
 CFG = dict(name='stak-notebooks',
            repo='https://github.com/spacetelescope',
            rev=version.__version__,
-           ext='.tar.gz')
+           ext='.tar.gz',
+           tmpdir='',
+           verbose=False)
+
 
 def download(url, destdir):
     bsize = 4096
@@ -30,41 +34,62 @@ def download(url, destdir):
     return filename
 
 
+@atexit.register
+def cleanup():
+    tmpdir = CFG['tmpdir']
+    if tmpdir and os.path.exists(tmpdir):
+        if CFG['verbose']:
+            print('Removing {}'.format(tmpdir))
+        shutil.rmtree(tmpdir)
+
+
 def main():
     import argparse
+    global CFG
     global verbose
 
     parser = argparse.ArgumentParser()
     parser.add_argument('-l', '--latest', action='store_true', help='Ignore current release and download the latest available')
-    parser.add_argument('-o', '--output-dir', action='store', type=str, default='.')
+    parser.add_argument('-o', '--output-dir', action='store', type=str, default=os.curdir)
     parser.add_argument('-f', '--force', action='store_true', help='Overwrite existing notebook directory')
     parser.add_argument('-v', '--verbose', action='store_true')
     args = parser.parse_args()
 
+    CFG['verbose'] = args.verbose
+
+    # Pull from HEAD (only useful for active development)
     if args.latest:
         CFG['rev'] = 'master'
 
-    verbose = args.verbose
+    # If the user issues an output directory, create the directory if it does not exist
     args.output_dir = os.path.abspath(args.output_dir)
-    expected = os.path.abspath(os.path.join(args.output_dir, '-'.join([CFG['name'], CFG['rev']])))
+    if not os.path.exists(args.output_dir):
+        os.makedirs(args.output_dir, mode=0o755)
 
+    # Do not clobber existing notebooks if they exist
+    expected = os.path.abspath(os.path.join(args.output_dir, '-'.join([CFG['name'], CFG['rev']])))
     if os.path.exists(expected) and not args.force:
         print('{} exists.\nUse --force to overwrite.'.format(expected), file=sys.stderr)
         exit(1)
 
-    with tempfile.TemporaryDirectory() as tdir:
-        url = '/'.join([CFG['repo'], CFG['name'], 'archive', CFG['rev'] + CFG['ext']])
+    # Create temporary directory and store location in configuration
+    CFG['tmpdir'] = tempfile.mkdtemp()
 
-        if verbose:
-            print('Retrieving {}'.format(url))
+    # Compile URL
+    url = '/'.join([CFG['repo'], CFG['name'], 'archive', CFG['rev'] + CFG['ext']])
 
-        archive = download(url, tdir)
+    # Download archive to temp directory
+    if CFG['verbose']:
+        print('Retrieving {}'.format(url))
+    archive = download(url, CFG['tmpdir'])
 
-        if verbose:
-            print('Unpacking {} to {}'.format(archive, args.output_dir))
+    # Extract archive in temp directory
+    if CFG['verbose']:
+        print('Unpacking {} to {}'.format(archive, args.output_dir))
+    shutil.unpack_archive(archive, args.output_dir)
 
-        shutil.unpack_archive(archive, args.output_dir)
+    # NOTE: cleanup() callback method deletes temporary directory
 
 
-
-
+if __name__ == '__main__':
+    main()
